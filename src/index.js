0e97fe5e555a5442ffe00e904ef155c560bbac4b
Davide D'Antonio <davide.dantonio@webeetle.com>
2017-12-01 07:36:12 +0100

    add sass

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import './index.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
